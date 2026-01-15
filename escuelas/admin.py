3b9ea06aa31b90d07137d9b24eeaf38c6d6b2290
Piero <pierocvargasc@gmail.com>
2026-01-14 23:48:43 -0500

    Modelo para gestionar la Escuela Profesional

--- escuelas/admin.py
+++ escuelas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Escuela
 
-# Register your models here.
+@admin.register(Escuela)
+class EscuelaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+    search_fields = ('nombre',)
