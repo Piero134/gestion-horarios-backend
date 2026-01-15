2bfcdeda84a66e9a2bd7ad21113fa097c81cc3fd
Piero <pierocvargasc@gmail.com>
2026-01-14 23:49:23 -0500

    Modelo para gestionar Planes de Estudio

--- planes/admin.py
+++ planes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import PlanEstudios
 
-# Register your models here.
+@admin.register(PlanEstudios)
+class PlanEstudiosAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'escuela')
+    list_filter = ('escuela',)
+    search_fields = ('nombre',)
--- planes/models.py
+++ planes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from escuelas.models import Escuela
 
-# Create your models here.
+class PlanEstudios(models.Model):
+    nombre = models.CharField(max_length=50)  # Plan 2018, Plan 2023
+    escuela = models.ForeignKey(
+        Escuela, on_delete=models.CASCADE, related_name='planes'
+    )
+
+    class Meta:
+        unique_together = ('nombre', 'escuela')
+
+    def __str__(self):
+        return f"{self.nombre} - {self.escuela}"
