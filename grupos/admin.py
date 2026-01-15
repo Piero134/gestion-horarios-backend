7a1fa96f65ea2c472964177d4e998f8461f38f8e
Piero <pierocvargasc@gmail.com>
2026-01-15 00:47:33 -0500

    Registro de grupos en admin

--- grupos/admin.py
+++ grupos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Grupo
+from horarios.admin import HorarioInline
 
-# Register your models here.
+@admin.register(Grupo)
+class GrupoAdmin(admin.ModelAdmin):
+    list_display = ('asignatura', 'nombre', 'periodo', 'docente', 'cupo_maximo')
+    list_filter = ('periodo__activo', 'periodo', 'asignatura__plan')
+    search_fields = ('asignatura__nombre', 'docente__apellido', 'nombre')
+    autocomplete_fields = ['asignatura', 'docente'] # Rapidez de búsqueda
+    inlines = [HorarioInline] # Para gestionar el tiempo y espacio aquí mismo
