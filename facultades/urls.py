90250b2bd4ce01d9ca6fb2c286166768d67383f7
Piero <pierocvargasc@gmail.com>
2026-02-14 20:46:09 -0500

    Api para filtrar departamentos por facultad

--- facultades/urls.py
+++ facultades/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import facultades_list
+from .views import facultades_list, api_load_departamentos
 from escuelas import views
 
 urlpatterns = [
     path('', facultades_list, name='facultades_list'),
     path('<int:facultad_id>/escuelas/', views.escuelas_list, name='escuelas_por_facultad'),
+    path('api/cargar-departamentos/', api_load_departamentos, name='api_load_departamentos'),
 ]
--- facultades/views.py
+++ facultades/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import Facultad
+from .models import Facultad, Departamento
+from django.http import JsonResponse
 
 @login_required
 def facultades_list(request):
@@ -13,3 +14,15 @@ def facultades_list(request):
     }
 
     return render(request, 'facultades_list.html', context)
+
+@login_required
+def api_load_departamentos(request):
+    facultad_id = request.GET.get('facultad_id')
+    if not facultad_id:
+        return JsonResponse({'departamentos': []})
+
+    # Filtramos departamentos que pertenecen a esa facultad
+    departamentos = Departamento.objects.filter(facultad_id=facultad_id).order_by('nombre')
+
+    data = [{'id': d.id, 'nombre': d.nombre} for d in departamentos]
+    return JsonResponse({'departamentos': data})
