0b64151fb05d8cc77b4c016a4edb09e3a40bac08
jackjimenes31 <jack.jimenes@unmsm.edu.pe>
2026-02-28 17:06:41 -0500

    Error: Aumentar max_length en facultades y escuelas para Postgres

--- facultades/models.py
+++ facultades/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Facultad(models.Model):
     nombre = models.CharField(
-        max_length=50,
+        max_length=150,
         unique=True,
         verbose_name="Nombre"
     )
